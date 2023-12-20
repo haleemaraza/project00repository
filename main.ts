fd9ee120199d31fb2b39456401f84723d84ee632
Haleema Raza <152488926+haleemaraza@users.noreply.github.com>
2023-12-20 14:49:14 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,47 @@
+import inquirer from "inquirer";
+
+const answer:{
+    numberOne:number,
+    numberTwo:number,
+    operator:string
+}=await inquirer.prompt([
+    {
+        type:"number",
+        name:"numberOne",
+        message:"kindly inter your first number:"
+
+    }, {
+        type:"number",
+        name:"numberTwo",
+        message:"kindly inter your second number:"
+
+    },  {
+        type:"list",
+        name:"operator",
+        choices:["addition(+)","subraction((-)","multiplication(*)","division(/)"],
+        message:"select your operator:"
+
+    },
+])
+
+const {numberOne,numberTwo,operator}=answer
+if (numberOne && numberTwo && operator){
+let result:number=0;
+    if (operator === "addition(+)"){
+        result=numberOne+numberTwo
+    }else if(operator === "subraction(-)"){
+        result=numberOne-numberTwo
+    }if (operator === "multiplication(*)"){
+        result=numberOne*numberTwo}
+        if (operator === "division(/)")
+            result=numberOne/numberTwo
+console.log("your result is ",result)}
+else{console.log("kindly enter valid input" )}
+
+
+
+
+
+
+
+
